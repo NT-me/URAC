50629d53553f4766c718fc48652c234bb550a92b
gnouf1 <jeuxforum26@gmail.com>
2020-03-15 04:15:27 +0100

    OK

--- my_lexer.py
+++ my_lexer.py
@@ -22,18 +22,39 @@ def lexe(listParse):
     JEU = load_jeu()
     erreur = False
     res = list()
-
+    erreur0 = False
     for i in listParse:
         listI = i.split(" ")
         # Vérifie que l'opérande existe dans le jeu
         for operande in JEU:
             if not listI[0] in operande:
+                # Cas où l'opérande n'est pas trouvée
                 erreur = True
             else:
+                ite = 0
+                # Cas où l'opérande est trouvée
+                if len(operande) != len(listI):
+                    erreur = True
+                    break
+                else:
+                    # Cas où il y'a le bon nombre de paramètre
+                    for p in operande:
+                        if p[:1] == 'R' and listI[ite][:1] != 'R':
+                            # Cas où il devrait y avoir un registre mais il n'y en a pas
+                            erreur0 = True
+                            break
+                        if p[:1] != 'R' and listI[ite][:1] == 'R':
+                            # Cas où il devrait y avoir un offset mais il y a un registre
+                            erreur0 = True
+                            break
+                        ite = ite + 1
+
                 erreur = False
+                if erreur0:
+                    break
                 break
 
-        if erreur:
+        if erreur or erreur0:
             print("Erreur de synthaxe ligne : \n => {}".format(i))
             quit()
         else :
