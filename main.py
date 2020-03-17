99b544d874ba5002b5962aacce01152383ed01ac
gnouf1 <jeuxforum26@gmail.com>
2020-03-18 00:49:07 +0100

    JUMP et BNEZ fonctionnel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import my_parser as P
 import my_lexer as L
 import code_generator as CG
+import time
 
 nom_du_jeu = "RISC_UVSQ"
 NB_CASE = 1000
@@ -13,31 +14,46 @@ def exec_res(code):
 
     MEM = list()
     taille_prog = len(code)
-
     while ca < NB_CASE:
         MEM.append(None)
         ca = ca + 1
 
+    print("--- Python (approx) Conversion ---")
     while CO < taille_prog:
-        if 'JUMP' not in code[CO] or 'BNEZ' not in code[CO]:
+    #     print("CO " + str(CO))
+    #
+    #     lst = dir()
+    #     for i in lst:
+    #         if i[:1] == 'R':
+    #             print("> {} = {}".format(i, eval(i)))
+        print(code[CO])
+        if 'JUMP' not in code[CO] and 'BNEZ' not in code[CO]:
             exec(code[CO])
-        else :
+            CO = CO + 1
+        else:
             if 'JUMP' in code[CO]:
                 J = code[CO].split(" ")
                 CO = CO + (int(eval(J[1])) + int(J[2]))
             elif 'BNEZ' in code[CO]:
                 B = code[CO].split(" ")
-                if eval(code[1]) != 0:
-                    CO = CO + (int(eval(B[0])) + int(B[2]))
-        print(code[CO])
+                if eval(B[2]) != 0:
+                    CO = int(CO) + (int(eval(B[1])) + int(B[3])/2)
+                    CO = int(CO)
+                    # print("> R4 = {}".format(R4))
+                    # print("> CO : {}".format(CO))
+                    # print("> B[2] " + str(eval(B[2])))
+                else:
+                    CO = CO + 1
 
-        CO = CO + 1
+        time.sleep(1)
 
+    print("--- --- ---\n\n")
     lst = dir()
 
     for i in lst:
         if i[:1] == 'R':
             print("{} = {}".format(i, eval(i)))
+
 
     print("--- WORD(s) IN MEMORY ---")
 
